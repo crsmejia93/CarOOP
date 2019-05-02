a5d39e771ec7786d94175efe69d1397e45e26667
Cesar Mejia <crsmejia93@gmail.com>
2019-05-02 13:13:08 -0400

    Added functionality for user to input cars

--- src/Car.java
+++ src/Car.java
@@ -1,11 +1,37 @@
 public class Car extends Vehicle {
+    private String year;
+    private String make;
     private String model;
 
+    public Car(){}
+
+    public Car(String year, String make, String model, String color){
+        this.year=year;
+        this.make=make;
+        this.model=model;
+        super.setColor(color);
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
 
     @Override
     public void setSpeed(int speed) {
         super.setSpeed(speed);
         System.out.println("The Speed is now "+speed+" mph");
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
     }
 
     public String getModel() {
@@ -30,6 +56,10 @@ public class Car extends Vehicle {
 
     public String stop(){
         return "The car is stopping";
+    }
+    @Override
+    public String toString(){
+        return "Year: "+getYear()+"\nMake: "+getMake()+"\nModel: "+getModel()+"\nColor: "+getColor()+"\n";
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
-        Car c1 = new Car();
-        c1.setColor("Red");
-        c1.setModel("911");
-        c1.start();
-        print(c1.accelerate());
-        c1.setSpeed(60);
-        print(c1.emisionTest());
-        print(c1.stop());
-
-        System.out.println();
-
-        Car c2 = new Car();
-        c2.setColor("Gray");
-        c2.setModel("Civic");
-        c2.start();
-        print(c2.accelerate());
-        c2.setSpeed(60);
-        print(c1.stop());
+        boolean quit = false;
+        Scanner input = new Scanner(System.in);
+        String carInfo;
+        String[] carInfoArr;
+        ArrayList<Car> carList = new ArrayList<>();
+        while(!quit){
+            System.out.print("Enter car information(year, make, model, color) or (q-quit): ");
+            carInfo = input.nextLine();
+            if(carInfo.equalsIgnoreCase("q")){
+                quit=true;
+            }
+            if(!carInfo.equalsIgnoreCase("q")){
+                carInfoArr = carInfo.split(", ");
+                carList.add( new Car(carInfoArr[0], carInfoArr[1],carInfoArr[2],carInfoArr[3]));
+            }
+        }
+        int i=1;
+        System.out.println("List of Cars");
+        for(Car car: carList){
+            System.out.println("Car " + i++);
+            System.out.println(car.toString());
+        }
     }
 
     private static void print(String s){
